92533df7ecc642922fbe170cab48693bcad60552
Roma Knyaz <git@knyaz.tech>
2023-10-14 00:33:58 +0200

    Finish up bodies + a few tests

--- cotix/_bodies.py
+++ cotix/_bodies.py
@@ -1,5 +1,4 @@
 import equinox as eqx
-import jax
 import jax.numpy as jnp
 from equinox import AbstractVar
 from jaxtyping import Array, Float
@@ -21,14 +20,13 @@ class AbstractBody(eqx.Module, strict=True):
     angular_velocity: AbstractVar[Float[Array, "2"]]
 
     shape: AbstractVar[AbstractShape]
+    _transform: AbstractVar[HomogenuousTransformer]
 
-    def get_transform(self):
+    def update_transform(self):
         """
         Construct a matrix that transforms shape in the correct configuration.
         While we could do it smarter, like cache the results somehow or whatever,
         we don't do that. Cuz, in theory, caching results is worse for complex shapes
-
-        So we return the homogenuous transform matrix
         """
         matrix = jnp.array(
             [
@@ -37,8 +35,15 @@ class AbstractBody(eqx.Module, strict=True):
                 [0.0, 0.0, 1.0],
             ]
         )
-        jax.debug.print("Transform matrix is {transform}", transform=matrix)
-        return HomogenuousTransformer(matrix)
+
+        return eqx.tree_at(lambda x: x._transform, self, HomogenuousTransformer(matrix))
+
+    def get_transform(self):
+        return eqx.error_if(
+            self._transform,
+            self._transform is None,
+            "Call update_transform before doing anything with an updated object",
+        )
 
     def collides_with(self, other, key):
         return check_for_collision(
@@ -48,6 +53,29 @@ class AbstractBody(eqx.Module, strict=True):
             trA=self.get_transform(),
             trB=other.get_transform(),
         )
+
+    def set_mass(self, mass):
+        return eqx.tree_at(lambda x: x.mass, self, mass)
+
+    def set_inertia(self, inertia):
+        return eqx.tree_at(lambda x: x.inertia, self, inertia)
+
+    def set_position(self, position):
+        new_self = eqx.tree_at(lambda x: x._transform, self, None)
+        return eqx.tree_at(lambda x: x.position, new_self, position)
+
+    def set_velocity(self, velocity):
+        return eqx.tree_at(lambda x: x.velocity, self, velocity)
+
+    def set_angle(self, angle):
+        new_self = eqx.tree_at(lambda x: x._transform, self, None)
+        return eqx.tree_at(lambda x: x.angle, new_self, angle)
+
+    def set_angular_velocity(self, angular_velocity):
+        return eqx.tree_at(lambda x: x.angular_velocity, self, angular_velocity)
+
+    def set_shape(self, shape):
+        return eqx.tree_at(lambda x: x.shape, self, shape)
 
 
 class Ball(AbstractBody, strict=True):
@@ -61,6 +89,7 @@ class Ball(AbstractBody, strict=True):
     angular_velocity: Float[Array, "2"]
 
     shape: AbstractShape
+    _transform: HomogenuousTransformer
 
     def __init__(self):
         self.mass = 1.0
@@ -70,6 +99,4 @@ class Ball(AbstractBody, strict=True):
         self.angle = 0
         self.angular_velocity = 0
         self.shape = Circle(0.05, jnp.zeros((2,)))
-
-    def move_to(self, target):
-        return eqx.tree_at(lambda x: x.position, self, target)
+        self._transform = HomogenuousTransformer()
--- test/test_bodies.py
+++ test/test_bodies.py
@@ -7,20 +7,43 @@ from cotix._shapes import Circle
 
 def test_ball_colliding():
     key = jr.PRNGKey(0)
-    a = Ball().move_to(jnp.array([0.08, 0.0]))
-    b = Ball().move_to(jnp.array([0.02, 0.0]))
+    a = Ball().set_position(jnp.array([0.08, 0.0]))
+    b = Ball().set_position(jnp.array([0.02, 0.0]))
+    a = a.update_transform()
+    b = b.update_transform()
     assert a.collides_with(b, key)[0] & b.collides_with(a, key)[0]
 
-    a = a.move_to(jnp.array([1.0, 0.0]))
+    a = a.set_position(jnp.array([1.0, 0.0]))
+    a = a.update_transform()
     assert (~b.collides_with(a, key)[0]) & (~a.collides_with(b, key)[0])
 
 
 def test_misaligned_ball_rotating():
     key = jr.PRNGKey(0)
-    a = Ball().move_to(jnp.array([0.0, 0.0]))
-    a = eqx.tree_at(lambda x: x.shape, a, Circle(jnp.array([10.0, 0.0]), 0.05))
-    a = eqx.tree_at(lambda x: x.angle, a, jnp.array(-jnp.pi / 2))
+    a = Ball().set_position(jnp.array([0.0, 0.0]))
+    a = eqx.tree_at(lambda x: x.shape, a, Circle(0.05, jnp.array([10.0, 0.0])))
+    a = eqx.tree_at(lambda x: x.angle, a, jnp.array(jnp.pi / 2))
 
-    b = Ball().move_to(jnp.array([0.0, 10.0]))
+    b = Ball().set_position(jnp.array([0.0, 10.0]))
 
-    assert b.collides_with(a, key)[0]
+    a = a.update_transform()
+    b = b.update_transform()
+
+    assert b.collides_with(a, key)[0] & a.collides_with(b, key)[0]
+
+
+def test_misaligned_ball_rotating_jitted():
+    @eqx.filter_jit
+    def f():
+        key = jr.PRNGKey(0)
+        a = Ball().set_position(jnp.array([0.0, 0.0]))
+        a = eqx.tree_at(lambda x: x.shape, a, Circle(0.05, jnp.array([10.0, 0.0])))
+        a = eqx.tree_at(lambda x: x.angle, a, jnp.array(jnp.pi / 2))
+
+        b = Ball().set_position(jnp.array([0.0, 10.0]))
+        a = a.update_transform()
+        b = b.update_transform()
+
+        return b.collides_with(a, key)[0] & a.collides_with(b, key)[0]
+
+    assert f()
