d1517598ea0c6774c11f59fb68446f36c512df95
Roma Knyaz <git@knyaz.tech>
2023-10-14 01:48:39 +0200

    Add invariant + apply it to the AbstractBody

--- cotix/_bodies.py
+++ cotix/_bodies.py
@@ -5,10 +5,12 @@ from jaxtyping import Array, Float
 
 from ._abstract_shapes import AbstractShape
 from ._collisions import check_for_collision
+from ._design_by_contract import class_invariant
 from ._geometry_utils import HomogenuousTransformer
 from ._shapes import Circle
 
 
+@class_invariant
 class AbstractBody(eqx.Module, strict=True):
     mass: AbstractVar[Float[Array, ""]]
     inertia: AbstractVar[Float[Array, ""]]
@@ -28,21 +30,20 @@ class AbstractBody(eqx.Module, strict=True):
         While we could do it smarter, like cache the results somehow or whatever,
         we don't do that. Cuz, in theory, caching results is worse for complex shapes
         """
-        matrix = jnp.array(
-            [
-                [jnp.cos(self.angle), -jnp.sin(self.angle), self.position[0]],
-                [jnp.sin(self.angle), jnp.cos(self.angle), self.position[1]],
-                [0.0, 0.0, 1.0],
-            ]
-        )
 
-        return eqx.tree_at(lambda x: x._transform, self, HomogenuousTransformer(matrix))
+        return eqx.tree_at(
+            lambda x: x._transform,
+            self,
+            replace=HomogenuousTransformer(position=self.position, angle=self.angle),
+        )
 
     def get_transform(self):
         return eqx.error_if(
             self._transform,
-            self._transform is None,
-            "Call update_transform before doing anything with an updated object",
+            (~jnp.all(self._transform.angle == self.angle))
+            | (~jnp.all(self._transform.position == self.position)),
+            "Call update_transform on a Body, before "
+            "doing any smart other things with it.",
         )
 
     def collides_with(self, other, key):
@@ -76,6 +77,23 @@ class AbstractBody(eqx.Module, strict=True):
 
     def set_shape(self, shape):
         return eqx.tree_at(lambda x: x.shape, self, shape)
+
+    def __invariant__(self):
+        return (
+            # Checks for nans
+            jnp.any(jnp.isnan(self.mass))
+            | jnp.any(jnp.isnan(self.inertia))
+            | jnp.any(jnp.isnan(self.position))
+            | jnp.any(jnp.isnan(self.velocity))
+            | jnp.any(jnp.isnan(self.angle))
+            | jnp.any(jnp.isnan(self.angular_velocity))
+            # Checks for some custom constraints
+            | (self.shape is None)
+            | jnp.any(self.mass < 0)
+            | jnp.any(self.inertia < 0)
+            | jnp.any(self.angle < -jnp.pi)
+            | jnp.any(self.angle > jnp.pi)
+        )
 
 
 class Ball(AbstractBody, strict=True):
