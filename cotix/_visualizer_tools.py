6bbef96573148116d7fa870e36f4c58c6d1f951d
ivrolan <ivanlpzbrc01@gmail.com>
2023-10-17 18:22:36 +0200

    removed t, fixed linting
    Signed-off-by: ivrolan <ivanlpzbrc01@gmail.com>

--- cotix/_visualizer_tools.py
+++ cotix/_visualizer_tools.py
@@ -1,17 +1,20 @@
 import abc
 
 import jax.numpy as jnp
-
 import pygame
 import pygame.locals
+
 from ._bodies import Ball
+
 
 class AbstractDrawer:
     @abc.abstractmethod
-    def draw_at_t(self, window, t, pix2meter):
+    def draw(self, window, pix2meter):
         """
-        Draw the object (e.g. a body) in the selected pygame window at a selected moment of time
-        This will typically call pygame.draw() methods based on the properties of the object
+        Draw the object (e.g. a body) in the selected pygame window at a selected
+        moment of time.
+        This will typically call pygame.draw() methods based on the properties of
+        the object
         """
 
         raise NotImplementedError
@@ -22,52 +25,55 @@ class BallDrawer(AbstractDrawer):
     color: pygame.Color
     # the list of visualization properties will change depending on the object to draw
 
-
-    def __init__(self, obj : Ball, c : pygame.Color):
+    def __init__(self, obj: Ball, c: pygame.Color):
         self.object = obj
         self.color = c
-    def draw_at_t(self, window, t, pix2meter):
-        
+
+    def draw(self, window, pix2meter):
         center_x, center_y = self.object.position[0], self.object.position[1]
-        # retrieve the first coord of the global support as it should be equal to the radius
-        # for more complex shapes we will need more info (get 360 points around a circle with the global_support?)
-        radius = self.object.shape.get_global_support(jnp.array([1.0, 0.]))[0]
-        
+        # retrieve the first coord of the global support as it should be equal
+        # to the radius
+        # for more complex shapes we will need more info (get 360 points around
+        # a circle with the global_support?)
+        radius = self.object.shape.get_global_support(jnp.array([1.0, 0.0]))[0]
+
         # convert meters to pixels with scale
         center_x *= pix2meter
         center_y *= pix2meter
         radius *= pix2meter
-        pygame.draw.circle(window, self.color, (center_x, center_y), radius)
+
+        pygame.draw.circle(
+            window, self.color, (int(center_x), int(center_y)), int(radius)
+        )
 
 
-class Visualizer():
-
-    def __init__(self, pix2meter_ratio, width_m, height_m, elements=[], name="Cotix Visualizer") -> None:
-    
+class Visualizer:
+    def __init__(
+        self, pix2meter_ratio, width_m, height_m, elements=[], name="Cotix Visualizer"
+    ) -> None:
         pygame.init()
         pygame.display.set_caption(name)
-        
-        self.window = pygame.display.set_mode((width_m*pix2meter_ratio, height_m*pix2meter_ratio))
+
+        self.window = pygame.display.set_mode(
+            (width_m * pix2meter_ratio, height_m * pix2meter_ratio)
+        )
         self.elements = elements
 
         self.pix2meter = pix2meter_ratio
 
-
         # Fill the screen with white color
         self.window.fill((255, 255, 255))
-        
-    
+
     # add element to draw
-    def add_element(self, element : AbstractDrawer):
+    def add_element(self, element: AbstractDrawer):
         self.elements.append(element)
-    
+
     # return the list of events that happened since the last update as pygame
     def get_events(self):
         return pygame.event.get()
-    
-    def draw_at_t(self, t):
 
+    def draw(self):
         for e in self.elements:
-            e.draw_at_t(self.window, t, self.pix2meter)
+            e.draw(self.window, self.pix2meter)
 
         pygame.display.flip()
--- test/test_viz.py
+++ test/test_viz.py
@@ -1,40 +1,42 @@
-import jax
-from jax import numpy as jnp, random as jr
-
+import jax.numpy as jnp
 import pygame
 import pygame.locals
 
 from cotix._bodies import Ball
-
-from cotix._visualizer_tools import *
+from cotix._visualizer_tools import BallDrawer, Visualizer
 
 
 def test_viz():
-
     framerate = 60
 
     viz = Visualizer(100, 6, 6, name="Test Viz")
 
     b = Ball()
+
+    b.set_position(b.position + jnp.array([3.0, 3.0]))
+    # b.update_transform()
+
+    print(b.position)
+
     b_drawer = BallDrawer(b, pygame.Color(255, 0, 0))
+
+    viz.add_element(b_drawer)
 
     run = True
 
-    # to visualize we will use a discretize t
-    t = 0
     while run:
-        
         # Iterating over all the events received from
         # pygame.event.get()
         for event in viz.get_events():
-    
             # If the type of the event is quit
             # then setting the run variable to false
             if event.type == pygame.locals.QUIT:
                 run = False
         # Draws the surface object to the screen.
 
-        # static ball for now, the properties of the Ball should change over time        
-        viz.draw_at_t(t)
-
+        # static ball for now, the properties of the Ball should change over time
+        viz.draw()
+        # b_drawer.object.set_position(b.position + jnp.array([0.1, 0.1]))
+        # print(b_drawer.object.position)
+        # print("-------------------")
         pygame.time.Clock().tick(framerate)
