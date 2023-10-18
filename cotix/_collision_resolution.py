0a70546e12b33c93ad3e8bc6ca5ad2a9c1e87ab7
Mikhail Vlasenko <mikhail.v.vlasenko@gmail.com>
2023-10-18 02:02:14 +0200

    elastic collision resolution for AbstractBody. epsilon removed

--- cotix/_collision_resolution.py
+++ cotix/_collision_resolution.py
@@ -1,7 +1,7 @@
 from jax import numpy as jnp
 from jaxtyping import Array, Float
 
-from cotix._bodies import AbstractBody, Ball
+from cotix._bodies import AbstractBody
 from cotix._geometry_utils import perpendicular_vector
 
 
@@ -9,14 +9,11 @@ def _split_bodies(
     body1: AbstractBody, body2: AbstractBody, epa_vector: Float[Array, "2"]
 ) -> (AbstractBody, AbstractBody):
     # lets apply translation to both bodies, taking their mass into account
-    epsilon = 1e-3
     body1 = body1.set_position(
-        body1.position
-        + (1 + epsilon) * epa_vector * (body2.mass / (body1.mass + body2.mass))
+        body1.position + epa_vector * (body2.mass / (body1.mass + body2.mass))
     )
     body2 = body2.set_position(
-        body2.position
-        - (1 + epsilon) * epa_vector * (body1.mass / (body1.mass + body2.mass))
+        body2.position - epa_vector * (body1.mass / (body1.mass + body2.mass))
     )
     return body1, body2
 
@@ -26,32 +23,16 @@ def _resolve_collision(
 ) -> (AbstractBody, AbstractBody):
     # todo: we need to determine the elasticity of the collision.
     #  probably based on the body properties
+    # todo: angular momentum
 
-    # for now, the collisions are inelastic
-    # calculate new velocities
-    momentum1 = body1.mass * body1.velocity
-    momentum2 = body2.mass * body2.velocity
-    new_velocity = (momentum1 + momentum2) / (body1.mass + body2.mass)
-    body1.velocity = new_velocity
-    body2.velocity = new_velocity
+    # todo: check that bodies are moving towards each other
+    # if jnp.dot(body1.velocity - body2.velocity, epa_vector) >= jnp.zeros(1):
+    #     return body1, body2
 
-    body1, body2 = _split_bodies(body1, body2, epa_vector)
-    return body1, body2
-
-
-def _1d_elastic_collision_velocities(m1, m2, u1, u2):
-    v1 = ((m1 - m2) / (m1 + m2)) * u1 + ((2 * m2) / (m1 + m2)) * u2
-    v2 = ((2 * m1) / (m1 + m2)) * u1 + ((m2 - m1) / (m1 + m2)) * u2
-    return v1, v2
-
-
-def _resolve_balls_collision(
-    body1: Ball, body2: Ball, epa_vector: Float[Array, "2"]
-) -> (Ball, Ball):
     elasticity = 1
 
     # change coordinate system from (x, y) to (q, r)
-    # where q is the line between the centers of the balls
+    # where q is the line along epa_vector and r is perpendicular to it
     unit_collision_vector = epa_vector / jnp.linalg.norm(epa_vector)
     perpendicular = perpendicular_vector(unit_collision_vector)
     change_of_basis = jnp.array([unit_collision_vector, perpendicular])
@@ -77,3 +58,9 @@ def _resolve_balls_collision(
 
     body1, body2 = _split_bodies(body1, body2, epa_vector)
     return body1, body2
+
+
+def _1d_elastic_collision_velocities(m1, m2, u1, u2):
+    v1 = ((m1 - m2) / (m1 + m2)) * u1 + ((2 * m2) / (m1 + m2)) * u2
+    v2 = ((2 * m1) / (m1 + m2)) * u1 + ((m2 - m1) / (m1 + m2)) * u2
+    return v1, v2
--- test/test_collision_resolution.py
+++ test/test_collision_resolution.py
@@ -2,7 +2,7 @@ import jax
 from jax import numpy as jnp, random as jr
 
 from cotix._bodies import Ball
-from cotix._collision_resolution import _resolve_balls_collision
+from cotix._collision_resolution import _resolve_collision
 from cotix._collisions import (
     check_for_collision_convex,
     compute_penetration_vector_convex,
@@ -12,6 +12,7 @@ from cotix._universal_shape import UniversalShape
 
 
 def test_circle_hits_circle_elastic():
+    # this test is kinda weak: i dont test that the speeds are fully correct
     key = jr.PRNGKey(0)
 
     def f(key):
@@ -38,25 +39,32 @@ def test_circle_hits_circle_elastic():
         res_first_collision, simplex = check_for_collision_convex(
             shape1.get_support, shape2.get_support, dir
         )
-        penetration = compute_penetration_vector_convex(
+        penetration1 = compute_penetration_vector_convex(
             shape1.get_support, shape2.get_support, simplex
         )
 
-        body1, body2 = _resolve_balls_collision(body1, body2, penetration)
+        body1, body2 = _resolve_collision(body1, body2, penetration1)
 
-        # i really dont like to do it this way
         total_position1 = body1.shape.parts[0].position + body1.position
         total_position2 = body2.shape.parts[0].position + body2.position
         new_shape1 = Circle(body1.shape.parts[0].radius, total_position1)
         new_shape2 = Circle(body2.shape.parts[0].radius, total_position2)
+        dir = new_shape1.get_center() - new_shape2.get_center()
         res_collision, simplex = check_for_collision_convex(
             new_shape1.get_support, new_shape2.get_support, dir
+        )
+        penetration2 = compute_penetration_vector_convex(
+            new_shape1.get_support, new_shape2.get_support, simplex
         )
 
         # should be no collision and velocities away from another ball
         v1_away = jnp.dot(body1.velocity, total_position2 - total_position1) <= 0
         v2_away = jnp.dot(body2.velocity, total_position1 - total_position2) <= 0
-        return ~res_collision & v1_away & v2_away & res_first_collision
+        no_collision = jnp.logical_or(
+            ~res_collision,
+            jnp.linalg.norm(penetration2) < 1e-3 * jnp.linalg.norm(penetration1),
+        )
+        return no_collision & v1_away & v2_away & res_first_collision
 
     N = 1000
     f = jax.vmap(f)
